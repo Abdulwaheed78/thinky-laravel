7cab433b3922cf6da298a3d1d5bed84f4f05a438
Abdul Waheed Shakir Ali Chaudhary <101925142+Abdulwaheed78@users.noreply.github.com>
2024-11-14 19:01:24 +0530

    laravel setup and database migrations done

---
+++ database/migrations/2024_11_14_122413_create-subscribes-table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+     /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('subscribes', function (Blueprint $table) {
+            $table->id();
+            $table->integer('blog_id')->default(0);
+            $table->string('name');
+            $table->string('email')->nullable();
+            $table->string('is_deleted')->default('no');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('subscribes');
+    }
+};
